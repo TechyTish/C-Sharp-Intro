efdd8521e15ab08b631aa31e50e75310d7398097
Tish Hutchinson <tish.hutchinson@slimmingworld.co.uk>
2021-11-11 14:44:22 +0000

    draw triangle shape complete

---
+++ Draw Shape/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Draw_Shape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print the following strings to draw a shape
+            Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /___|");
+
+            //leave console window open
+            Console.ReadLine();
+        }
+    }
+}
