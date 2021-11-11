32681f53f5c462ebdf019fe9c1bf6ed58355035a
Tish Hutchinson <tish.hutchinson@slimmingworld.co.uk>
2021-11-11 14:57:53 +0000

    data types complete

--- Data Types/Program.cs
+++ Data Types/Program.cs
@@ -6,8 +6,26 @@ namespace Data_Types
     {
         static void Main(string[] args)
         {
-            //data types
+            //multiple characters
+            string phrase = "this is a string";
 
+            //one character
+            char result = 'A';
+
+            //integer (whole number)
+            int age = 27;
+
+            //float
+            float percent = 15;
+
+            //decimal
+            decimal precision = 12;
+
+            //double
+            double average = 3.0;
+
+            //boolean (true or false)
+            bool isFemale = true;
         }
     }
 }
